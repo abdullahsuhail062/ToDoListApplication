95ff606460c8b3f73bb461d138b51bd2f8c2445d
abdullahsuhail062 <abdullahsuhail062@gmail.com>
2026-01-13 22:26:49 +0500

    update

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,7 @@
 import { Component, signal } from '@angular/core';
 import { Router, RouterOutlet } from '@angular/router';
 import { AuthService } from './authFiles/auth.service';
+import { AuthStore } from './authFiles/auth-store';
 
 
 @Component({
@@ -13,13 +14,13 @@ export class App {
   protected readonly title = signal('ToDoListApplication');
 
 
-  constructor(private router: Router, private authService: AuthService) {}
+  constructor(private router: Router, private authStore: AuthStore) {}
 
   ngOnInit(): void {
     // Check on app initialization/resume
-    if (!this.authService.isLoggedIn()) {
+    if (!this.authStore.isLoggedIn()) {
       // If not logged in (token expired or not present), logout and navigate to login
-      this.authService.logout(); // logout now handles navigation to /login
+      this.authStore.clear() // logout now handles navigation to /login
     } else {
       // If logged in, navigate to dashboard
       this.router.navigate(['/dashboard']);
